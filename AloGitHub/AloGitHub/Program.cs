902966c05ceb1379e5e40cbc3b1f33c1aeac39ce
thiago milton ferreira <thiago.milton2009@hotmail.com>
2018-11-08 16:39:47 -0200

    Atualizei a  funçao Escreve na tela

--- AloGitHub/AloGitHub/Program.cs
+++ AloGitHub/AloGitHub/Program.cs
@@ -8,13 +8,16 @@ namespace AloGitHub
 {
     class Program
     {
-        static void EscreveNaTela()
+        static void EscreveNaTela(String txt)
         {
-            Console.WriteLine("Primeiro teste com o gitHub");
+            Console.WriteLine(txt);
         }
+     
+
+
         static void Main(string[] args)
         {
-            EscreveNaTela();
+            EscreveNaTela("Primeiro Programa com GItHub");
             Console.ReadKey();
 
         }
