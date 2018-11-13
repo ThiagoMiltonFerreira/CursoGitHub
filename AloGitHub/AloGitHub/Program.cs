0207f441bf3b2fdd4dbcb21d8b05755364245b7d
thiago milton ferreira <thiago.milton2009@hotmail.com>
2018-11-13 18:02:06 -0200

    Nova Funcao escreve na tela 2 atual

--- AloGitHub/AloGitHub/Program.cs
+++ AloGitHub/AloGitHub/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,13 @@ namespace AloGitHub
     {
         static void EscreveNaTela(String txt)
         {
-            Console.WriteLine(txt);
+           Console.WriteLine(txt);
         }
         static void EscreveNaTela2(String txt)
         {
-            EscreveNaTela2(txt);
+            EscreveNaTela(txt);
         }
-
+            
 
         static void Main(string[] args)
         {
